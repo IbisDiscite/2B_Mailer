1d4c41cb88081fe2c5e13d4d413dd82c3397d34a
Pedro Camilo Berrio Pinzon <pcberriop@unal.edu.co>
2018-05-02 10:43:04 -0500

    Crud full docker

--- Web/api/routes/message.js
+++ Web/api/routes/message.js
@@ -4,18 +4,35 @@ const mongoose = require('mongoose');
 
 const Message = require('../models/message');
 
-router.get('/', (req, res, next)=>{
-    res.status(200).json({
-        message: 'Handling GET requests to /messages'
-    });
+router.get('/', (req, res, next) => {
+    Message
+        .find()
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            // if (docs.length >= 0){
+            res.status(200).json(docs);
+            // } else {
+            //     res.status(404).json({
+            //         message: 'No entries found'
+            //     });
+            // }
+
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        })
 });
 
-router.post('/',(req, res, next)=>{
+router.post('/', (req, res, next) => {
     const message = new Message({
         _id: new mongoose.Types.ObjectId(),
         subject: req.body.subject,
         text: req.body.text
-});
+    });
     message
         .save()
         .then(result => {
@@ -32,20 +49,19 @@ router.post('/',(req, res, next)=>{
             });
         });
 
-
 });
 
-router.get('/:messageId', (req, res, next)=>{
+router.get('/:messageId', (req, res, next) => {
     const id = req.params.messageId;
     Message.findById(id)
         .exec()
         .then(doc => {
             console.log(doc);
-            if (doc){
+            if (doc) {
                 res.status(200).json(doc);
             } else {
                 res.status(404).json({
-                        message: 'No valid entry found for provided Id'
+                    message: 'No valid entry found for provided Id'
                 });
             }
         })
@@ -58,15 +74,38 @@ router.get('/:messageId', (req, res, next)=>{
 });
 
 router.patch('/:messageId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated message!'
-    });
+    const id = req.params.messageId;
+    const updateOps ={};
+    for (const ops of req.body){
+        updateOps[ops.propSubject] = ops.value;
+    }
+    Message.update({_id: id},{$set: updateOps })
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.delete('/:messageId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted message!'
-    });
+    const id = req.params.messageId;
+    Message.remove({_id: id})
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 
